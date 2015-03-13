77032846805674cdec84fae60f777f1e1f698c7a
Torsten Römer <torsten.roemer@luniks.net>
2015-03-13 22:16:43 +0100

    - Included the module USART in the project - Added GPL and LGPL licenses - Added attribution for USART and Makefile - Updated Makefiles

--- lambda/adc.c
+++ lambda/adc.c
@@ -4,6 +4,27 @@
  *  Created on: 11.03.2015
  *      Author: dode@luniks.net
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Alternatively, the contents of this file may be used under the terms of the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL License"), in which
+ * case the provisions of the LGPL License are applicable instead of those
+ * above.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU (Lesser) General Public License for more details.
+ *
+ * DISCLAIMER: I'm experienced with Java, but new to C.
+ *
+ * ATTRIBUTION: This project includes the module USART and the Makefile from
+ * the code accompanying the book Make: AVR Programming by Elliot Williams,
+ * a great book and a pleasant read, that helped me tremendously to get
+ * started with AVR programming.
  */
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
@@ -15,19 +36,26 @@
 // #define TEMPO_OP_OFFSET_MV 441
 #define TEMPO_OP_OFFSET_MV 454
 
+// TODO what exactly does this do?
 EMPTY_INTERRUPT(ADC_vect);
 
 void setupADC(void) {
-	ADMUX |= (1 << REFS0); // use AVCC as reference voltage
-	// ADCSRA |= (1 << ADPS1) | (1 << ADPS2); // ADC clock prescaler /64
-	ADCSRA |= (1 << ADPS2); // ADC clock prescaler /16
-	ADCSRA |= (1 << ADEN); // enable ADC
+	// use AVCC as reference voltage
+	ADMUX |= (1 << REFS0);
+	// ADC clock prescaler/64
+	// ADCSRA |= (1 << ADPS1) | (1 << ADPS2);
+	// ADC clock prescaler/16
+	ADCSRA |= (1 << ADPS2);
+	// enable ADC
+	ADCSRA |= (1 << ADEN);
 }
 
 void setupSleepMode(void) {
 	set_sleep_mode(SLEEP_MODE_ADC);
-	ADCSRA |= (1 << ADIE); // enable ADC interrupt
-	sei(); // enable global interrupts
+	// enable ADC interrupt
+	ADCSRA |= (1 << ADIE);
+	// enable global interrupts
+	sei();
 }
 
 int16_t getVoltage(uint8_t port) {
--- lambda/integers.c
+++ lambda/integers.c
@@ -3,6 +3,28 @@
  *
  *  Created on: 07.03.2015
  *      Author: dode@luniks.net
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Alternatively, the contents of this file may be used under the terms of the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL License"), in which
+ * case the provisions of the LGPL License are applicable instead of those
+ * above.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU (Lesser) General Public License for more details.
+ *
+ * DISCLAIMER: I'm experienced with Java, but new to C.
+ *
+ * ATTRIBUTION: This project includes the module USART and the Makefile from
+ * the code accompanying the book Make: AVR Programming by Elliot Williams,
+ * a great book and a pleasant read, that helped me tremendously to get
+ * started with AVR programming.
  */
 #include <avr/io.h>
 #include "integers.h"
