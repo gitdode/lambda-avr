49264e2fb5f0ca4e88d38bf3eadffb0017bd8953
Torsten Römer <torsten.roemer@luniks.net>
2015-05-14 20:15:09 +0200

    use prototype pin assignment

--- lambda/pins.h
+++ lambda/pins.h
@@ -13,18 +13,18 @@
 /** ADC pin for the type K thermocouple signal */
 #define ADC_TEMPI PC5
 /** ADC pin for the PT1000 resistance thermometer signal */
-#define ADC_TEMPO PC0	// prototype: PC0
+#define ADC_TEMPO PC0	// diagram: PC4
 /** ADC pin for the LSM 11 oxygen sensor signal */
-#define ADC_LAMBDA PC2	// prototype: PC2
+#define ADC_LAMBDA PC2	// diagram: PC3
 
 /* Pins for the LCD */
 #define LCD_PORT PORTD
 #define LCD_DDR	 DDRD
-#define LCD_RS   PD6	// prototype: PD6
-#define LCD_EN   PD7	// prototype: PD7
+#define LCD_RS   PD6	// diagram: PD7
+#define LCD_EN   PD7	// diagram: PD6
 #define LCD_DB4  PD5
-#define LCD_DB5  PD2	// prototype: PD2
+#define LCD_DB5  PD2	// diagram: PD4
 #define LCD_DB6  PD3
-#define LCD_DB7  PD4	// prototype: PD4
+#define LCD_DB7  PD4	// diagram: PD2
 
 #endif /* PINS_H_ */
