07065bdc2eef0d391688f3657c12af6e2a18a5e2
Torsten Römer <torsten.roemer@luniks.net>
2015-05-30 00:10:32 +0200

    added basic i18n support and German translation

--- lambda/messages.h
+++ lambda/messages.h
@@ -8,22 +8,50 @@
 #ifndef MESSAGES_H_
 #define MESSAGES_H_
 
-/* lambda.c */
-#define MSG_WELCOME "     Hello!     "
+#ifndef LANG
+	#define LANG 0
+#endif
 
-/* sensors.c */
-#define MSG_LEAN "Lean "
-#define MSG_OKAY "Okay "
-#define MSG_IDEAL "Ideal"
-#define MSG_RICH "Rich!"
+#if LANG == 0
 
-/* rules.c */
-#define MSG_AIRGATE_50_0 "Air gate 50%"
-#define MSG_AIRGATE_25_0 "Air gate 25%"
-#define MSG_AIRGATE_CLOSE_0 "Close air gate"
-#define MSG_AIRGATE_CLOSE_1 "Turn off heating"
-#define MSG_TOO_RICH_0 "Too rich, open"
-#define MSG_TOO_RICH_1 "air gate!"
-#define MSG_FIRE_OUT_0 "Fire out?"
+	/* lambda.c */
+	#define MSG_WELCOME "     Hello!     "
+
+	/* sensors.c */
+	#define MSG_LEAN "Lean "
+	#define MSG_OKAY "Okay "
+	#define MSG_IDEAL "Ideal"
+	#define MSG_RICH "Rich!"
+
+	/* rules.c */
+	#define MSG_AIRGATE_50_0 "Air gate 50%"
+	#define MSG_AIRGATE_25_0 "Air gate 25%"
+	#define MSG_AIRGATE_CLOSE_0 "Close air gate"
+	#define MSG_AIRGATE_CLOSE_1 "Turn off heating"
+	#define MSG_TOO_RICH_0 "Too rich, open"
+	#define MSG_TOO_RICH_1 "air gate"
+	#define MSG_FIRE_OUT_0 "Fire out?"
+
+#elif LANG == 1
+
+	/* lambda.c */
+	#define MSG_WELCOME "     Hallo!     "
+
+	/* sensors.c */
+	#define MSG_LEAN "Mager"
+	#define MSG_OKAY "Okay "
+	#define MSG_IDEAL "Ideal"
+	#define MSG_RICH "Fett!"
+
+	/* rules.c */
+	#define MSG_AIRGATE_50_0 "Luftschieber 50%"
+	#define MSG_AIRGATE_25_0 "Luftschieber 25%"
+	#define MSG_AIRGATE_CLOSE_0 "Luftschieber zu"
+	#define MSG_AIRGATE_CLOSE_1 "Heiz. abschalten"
+	#define MSG_TOO_RICH_0 "Zu fett, Luft-"
+	#define MSG_TOO_RICH_1 "schieber oeffnen"
+	#define MSG_FIRE_OUT_0 "Feuer aus?"
+
+#endif
 
 #endif /* MESSAGES_H_ */
